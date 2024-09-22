88f611ff7122dfeb014be15383bc06bcc51d5e71
OmscarT <oscartorresramirez498@gmail.com>
2024-09-21 19:01:35 -0600

    impresion nombre

---
+++ mi_nombre.java
@@ -0,0 +1,12 @@
+package Pruebas_GIT;
+
+public class mi_nombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String N = "Oscar Torres Ramirez";
+		System.out.println("Torres Ramirez Oscar Joaquin |  "+ N);
+
+	}
+
+}
